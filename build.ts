d9521f0907219827fd9e2ddd783b72ac3b8c62b0
Binshad k <68679199+binshadcs@users.noreply.github.com>
2025-08-08 15:30:43 +0530

    refactor: type store module

---
+++ build.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import * as fs from 'fs';
+import { execSync } from 'child_process';
+
+
+function beforeRollup() {
+}
+
+function rollup() {
+  execSync('rollup -c', { encoding: 'utf-8' });
+}
+
+function afterRollup() {
+  // copy styles directory
+  fs.cpSync('./src/styles', './dist/styles', {recursive: true});
+}
+
+beforeRollup();
+rollup();
+afterRollup();
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  server: {
+    port: 8080
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@examples': resolve(__dirname, 'examples')
+    }
+  }
+})
